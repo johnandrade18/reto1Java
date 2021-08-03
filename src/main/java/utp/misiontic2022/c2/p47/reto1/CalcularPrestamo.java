9a7cd54808ed6da973b79db32b09f3bff45af83e
John Andrade <johnandrade18@hotmail.com>
2021-08-02 20:16:03 -0500

    Código Calcular PrestamoCorregido

--- src/main/java/utp/misiontic2022/c2/p47/reto1/CalcularPrestamo.java
+++ src/main/java/utp/misiontic2022/c2/p47/reto1/CalcularPrestamo.java
@@ -1,100 +1,62 @@
 package utp.misiontic2022.c2.p47.reto1;
 
 public class CalcularPrestamo {
-    //----------------------------------------------
-    //Atributos
-    private int pTiempo;
-    private double pCapital;
-    private double pInteres;
-    private double interesSimple;
-    private double interesCompuesto;
-    private double compararInversion;
-    //----------------------------------------------
 
-    //----------------------------------------------
-    //Constructores
-    public CalcularPrestamo() {
-        this.pCapital = 0.0;
-        this.pInteres = 0.0;
-        this.pTiempo = 0;
-    }
-    //----------------------------------------------
+  // Atributos
+  private int pTiempo;
+  private double pMonto;
+  private double pInteres;
+  private double diferencia;
+  private double interesSimple;
+  private double interesCompuesto;
 
-    //----------------------------------------------
-    //Metodos
-    public double interesSimples() {
-        return this.setInteresSimple((((pCapital * pInteres)/100) * pTiempo));
-    }
-    public double interesCompuestos() {
-        return this.setInteresCompuesto(((pCapital*((((1+pInteres)/100)*pTiempo)-1))));
-    }
-    public double compararInversion() {
-        return this.setCompararInversion(((this.interesCompuesto - this.interesSimple)));
-    }
-    
-    //----------------------------------------------
+  //Constructores
 
-    public double compararPrestamo(int pTiempo, double pCapital, double pInteres) {
-        CalcularPrestamo calcularPrestamo  =  new CalcularPrestamo();
-        
-        
-    }
+  public CalcularPrestamo(int pTiempo, double pMonto, double pInteres) {
+    this.pMonto = pMonto;
+    this.pInteres = pInteres;
+    this.pTiempo = pTiempo;
+  }
 
-    public int getpTiempo() {
-        return pTiempo;
-    }
+  public CalcularPrestamo() {
+    this.pMonto = 0;
+    this.pInteres = 0;
+    this.pTiempo = 0;
+  }
 
-    public void setpTiempo(int pTiempo) {
-        this.pTiempo = pTiempo;
-    }
+  // Métodos
+  public double calcularInteresSimple() {
+    this.interesSimple = this.pMonto * (this.pInteres / 100) * this.pTiempo;
+    return Math.round(interesSimple);
+  }
 
-    public double getpCapital() {
-        return pCapital;
-    }
+  public double calcularInteresCompuesto() {
+    this.interesCompuesto =
+      this.pMonto * ((Math.pow((1 + (this.pInteres / 100)), this.pTiempo)) - 1);
+    return Math.round(interesCompuesto);
+  }
 
-    public void setpCapital(double pCapital) {
-        this.pCapital = pCapital;
-    }
+  public double compararPrestamo(int pTiempo, double pMonto, double pInteres) {
+    this.pMonto = pMonto;
+    this.pInteres = pInteres;
+    this.pTiempo = pTiempo;
+    this.diferencia = calcularInteresCompuesto() - calcularInteresSimple();
+    return this.diferencia;
+  }
 
-    public double getpInteres() {
-        return pInteres;
-    }
+  public double compararPrestamo() {
+    this.diferencia = calcularInteresCompuesto() - calcularInteresSimple();
+    return this.diferencia;
+  }
 
-    public void setpInteres(double pInteres) {
-        this.pInteres = pInteres;
-    }
-    public double getInteresSimple() {
-        return interesSimple;
-    }
-
-    public double setInteresSimple(double interesSimple) {
-        this.interesSimple = interesSimple;
-        return interesSimple;
-    }
-
-    public double getInteresCompuesto() {
-        return interesCompuesto;
-    }
-
-    public double setInteresCompuesto(double interesCompuesto) {
-        this.interesCompuesto = interesCompuesto;
-        return interesCompuesto;
-    }
-    
-    public double getCompararInversion() {
-        return compararInversion;
-    }
-
-    public double setCompararInversion(double compararInversion) {
-        this.compararInversion = compararInversion;
-        return compararInversion;
-    }
-
-
-
-    public static void main(String[] args) {
-        System.out.println(resultadoInversion.calcularInteresSimple()); 
-        System.out.println(resultadoInversion.calcularInteresCompuesto());
-        System.out.println(resultadoInversion.compararPrestamo(24,5000000,4.2)); 
-    }
+  public static void main(String[] args) throws Exception {
+    CalcularPrestamo resultadoInversion = new CalcularPrestamo(
+      24,
+      5000000,
+      4.2
+    );
+    System.out.println(resultadoInversion.calcularInteresSimple());
+    System.out.println(resultadoInversion.calcularInteresCompuesto());
+    System.out.println(resultadoInversion.compararPrestamo(24, 5000000, 4.2));
+  }
 }
